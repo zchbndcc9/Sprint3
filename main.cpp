69d4b7e8947d30cb06b326ecd98b4e123897c4ab
MARK FONTENOT <zchbndcc9@gmail.com>
2017-10-11 10:35:46 -0700

    Added dsString * to assign dynamically allocated array to

--- main.cpp
+++ main.cpp
@@ -9,33 +9,35 @@ int main(int argc, char *argv[]) {
 
     Algorithms a;
     readandWrite rh;
-    dsVector<dsString> vec;
-    int wordsToSort;
+    dsString* arr;
+    int wordsToSort, totalWords;
 
     std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-    rh.readInput(vec, argv[1], wordsToSort);
+    rh.readInput(arr, argv[1], wordsToSort, totalWords);
+
     std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
     cout << duration << " microseconds" << endl;
 
     cout << "Sorting..." << endl;
     t1 = std::chrono::high_resolution_clock::now();
-    a.sortLength(vec, 0, vec.size()-1, wordsToSort);
+    a.sortLength(arr, 0, totalWords - 1, wordsToSort);
     t2 = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
 
      cout << "Sorted by length.  " << '\n' << "time: " << duration << " microseconds" << '\n';
 
-    t1 = std::chrono::high_resolution_clock::now();
-    a.sortString(vec, 0, vec.size()-1);
-    t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+//    t1 = std::chrono::high_resolution_clock::now();
+//    a.sortString(arr, 0, vec.size()-1);
+//    t2 = std::chrono::high_resolution_clock::now();
+//    duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
 
-    cout << "Sorted alphabetically.  " << '\n' << "time: " << duration << " microseconds" << '\n';
+//    cout << "Sorted alphabetically.  " << '\n' << "time: " << duration << " microseconds" << '\n';
 
-    t1 = std::chrono::high_resolution_clock::now();
-    rh.writeOutput(vec, argv[2], wordsToSort);
-    t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-    cout << duration << " microseconds" << endl;
+//    t1 = std::chrono::high_resolution_clock::now();
+//    rh.writeOutput(vec, argv[2], wordsToSort);
+//    t2 = std::chrono::high_resolution_clock::now();
+//    duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+//    cout << duration << " microseconds" << endl;
+     delete[] arr;
 }
