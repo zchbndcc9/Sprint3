650a68a5ef565de0c27c049ae7222c9fae3af70f
MARK FONTENOT <zchbndcc9@gmail.com>
2017-10-11 16:28:01 -0700

    test main

--- main.cpp
+++ main.cpp
@@ -12,32 +12,32 @@ int main(int argc, char *argv[]) {
     dsString* arr;
     int wordsToSort, totalWords;
 
-    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+    chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
     rh.readInput(arr, argv[1], wordsToSort, totalWords);
 
     std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-    cout << duration << " microseconds" << endl;
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+    cout << duration << " milliseconds" << endl;
 
     cout << "Sorting..." << endl;
     t1 = std::chrono::high_resolution_clock::now();
     a.sortLength(arr, 0, totalWords - 1, wordsToSort);
     t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
 
-     cout << "Sorted by length.  " << '\n' << "time: " << duration << " microseconds" << '\n';
+     cout << "Sorted by length.  " << '\n' << "time: " << duration << " milliseconds" << '\n';
 
-//    t1 = std::chrono::high_resolution_clock::now();
-//    a.sortString(arr, 0, vec.size()-1);
-//    t2 = std::chrono::high_resolution_clock::now();
-//    duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    t1 = std::chrono::high_resolution_clock::now();
+    a.sortString(arr, 0, totalWords-1);
+    t2 = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
 
-//    cout << "Sorted alphabetically.  " << '\n' << "time: " << duration << " microseconds" << '\n';
+    cout << "Sorted alphabetically.  " << '\n' << "time: " << duration << " milliseconds" << '\n';
 
-//    t1 = std::chrono::high_resolution_clock::now();
-//    rh.writeOutput(vec, argv[2], wordsToSort);
-//    t2 = std::chrono::high_resolution_clock::now();
-//    duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-//    cout << duration << " microseconds" << endl;
+    t1 = std::chrono::high_resolution_clock::now();
+    rh.writeOutput(arr, argv[2], wordsToSort);
+    t2 = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+    cout << duration << " milliseconds" << endl;
      delete[] arr;
 }
