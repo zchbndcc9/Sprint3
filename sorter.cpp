b0e8c4ad80cc9a60394ad1f6c7d2f9dd3ec7b6ba
MARK FONTENOT <zchbndcc9@gmail.com>
2017-10-12 18:27:16 -0500

    Merged readandparse

--- sorter.cpp
+++ sorter.cpp
@@ -1,6 +1,60 @@
 #include "sorter.h"
 
-Sorter::Sorter()
-{
+Sorter::Sorter() {
 
+}
+
+void Sorter::sort(){
+
+}
+
+void Sorter::readInput(char* input_file){
+
+    ifstream inFile(input_file);
+
+    //checks if dictFile isnt opening
+    if (!inFile){
+        cerr << input_file << " COULD NOT BE READ." << endl;
+        return;
+    }
+
+    inFile >> totalWords;
+    inFile >> wordsToSort;
+
+    s_ptr = new dsString*[totalWords];
+
+    dsString line;
+    int i = 0;
+    char buffer[80] ={0};
+
+    //while the file isnt at the end
+    while(!inFile.eof()){
+        inFile >> buffer;
+        s_ptr[i] = new dsString(buffer);
+        i++;
+    }
+
+    inFile.close();
+}
+
+void Sorter::writeOutput(char* output_file){
+    ofstream outFile(output_file);
+
+    if (!outFile)
+        cout << output_file << " COULD NOT BE OPENED" << endl;
+
+    for(int i = 0; i < wordsToSort; i++){
+        outFile << *(s_ptr[i]) << '\n';
+    }
+
+    outFile << endl;
+    outFile.close();
+}
+
+
+Sorter::~Sorter(){
+    for(int i = 0; i < totalWords; i++){
+        delete s_ptr[i];
+    }
+    delete[] s_ptr;
 }
